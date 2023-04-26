f1d4a6e34f9bda2c6f1aecd9f1bd3370c97fd39c
Daniel Koirala <adkdanielr@gmail.com>
2023-04-26 10:30:17 -0400

    added more questions

--- website/static/game.js
+++ website/static/game.js
@@ -12,7 +12,7 @@ let availableQuestions = [];
 
 let questions = [
   {
-    question: "Inside which HTML element do we put the JavaScript??",
+    question: "Inside which HTML element do we put the JavaScript?",
     choice1: "<script>",
     choice2: "<javascript>",
     choice3: "<js>",
@@ -79,10 +79,42 @@ let questions = [
     choice4: "8",
     answer: 4,
   },
+  {
+    question: " Which of the following is not a rule?",
+    choice1: "Strive for consistency",
+    choice2: "Reduce short-term memory load",
+    choice3: "Enable frequent shortcuts",
+    choice4: "Strive for simplicity",
+    answer: 4,
+  },
+  {
+    question: " Which of the following is not part of the CIA triad?",
+    choice1: "Availability",
+    choice2: "Confidentiality",
+    choice3: "Authentication",
+    choice4: "Integrity",
+    answer: 3,
+  },
+  {
+    question: " Which of the following is not a hash?",
+    choice1: "SHA-256",
+    choice2: "MI6",
+    choice3: "MD5",
+    choice4: "RipeMD",
+    answer: 2,
+  },
+  {
+    question: "What's Giovanni's Middle Name?",
+    choice1: "Villalobos",
+    choice2: "Herrera",
+    choice3: "Carrera",
+    choice4: "Santos",
+    answer: 1,
+  },
 ];
 
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 6;
 
 startGame = () => {
   questionCounter = 0;
